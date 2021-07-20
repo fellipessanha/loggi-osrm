900f237a55baad3960cb87405be736df17c50de4
Igor Machado <igor.machado@gmail.com>
2021-07-20 17:59:27 -0300

    fix functions

--- src/loggi-include.hpp
+++ src/loggi-include.hpp
@@ -5,23 +5,23 @@
 #include <OptFrame/Core.hpp>
 //
 using ESolutionVRP = std::pair<
-  std::vector<std::vector<int>>, optframe::Evaluation<double> 
-  >;
+  std::vector<std::vector<int>>,
+  optframe::Evaluation<double>>;
 //
 #include <fstream>
-#include <sstream>
 #include <set>
+#include <sstream>
 //
 #include "../nlohmann/json.hpp"
 using json = nlohmann::json;
 //
-#include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
+#include <curlpp/cURLpp.hpp>
 //
 #include "loggi-context-loader.hpp"
 //
 #include "loggi-evaluator.cpp"
 #include "loggi-solution-generators.cpp"
 //
-#include "FCore_functions.hpp"
+//#include "FCore_functions.hpp"
--- src/optframe-main.cpp
+++ src/optframe-main.cpp
@@ -1,15 +1,17 @@
 #include "loggi-include.hpp"
 
-int main()
+#include "FCore_functions.hpp"
+
+int
+main()
 {
-  // instance loading
-  loggibud::Instance instance("data/cvrp-0-rj-0.json", "data/rj-0.json");
-  auto &deliveriesList = instance.getAllDeliveries();
-  loggibud::FCoreLoggi fC_loggi(instance);
+   // The case with CTAD - Class Template Argument Deduction (C++20)
+   //std::vector ints{ 1, 2, 3, 4, 5, 6, 7 }; // deduced vector<int>
 
+   // instance loading
+   loggibud::Instance instance("data/cvrp-0-rj-0.json", "data/rj-0.json");
+   auto& deliveriesList = instance.getAllDeliveries();
+   loggibud::FCoreLoggi fC_loggi(instance);
 
-
- 
-
-  return 0;
+   return 0;
 }
